025bfd7b2b8f353e14601c8538e03de77305eb3d
alirezaporthos <alirezaportous@gmial.com>
2020-07-29 04:13:48 -0700

    dashboard changed

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -2,11 +2,14 @@
 
 @section('content')
 <div class="container">
+    
+    
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
+                
+                
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -14,8 +17,11 @@
                         </div>
                     @endif
                     
+                    <h1>Dashboard</h1>
+                    {{--
                     <a href="posts/create" class="btn btn-primary">Create Post</a>
                     <br></br>
+                    
                     @if(count($posts) > 0)
                     <h3>Your Posts</h3>
                     <table class='table table-striped'>
@@ -43,5 +49,6 @@
             </div>
         </div>
     </div>
+    --}}
 </div>
 @endsection
