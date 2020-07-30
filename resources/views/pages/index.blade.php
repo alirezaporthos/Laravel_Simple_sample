a16e0ba32eb3d5bf53fe227a247ba54c3cf5f9f7
alirezaporthos <alirezaportous@gmial.com>
2020-07-30 01:07:55 -0700

    some minor stuff fixed

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,7 +4,7 @@
 @section('content')
     <div class="jumbotron text-center">
         <h1>{{$title}}</h1>
-        <p>This is the Laravel application from Youtube</p>
+        <p>This is a Laravel application </p>
         <p><a class="btn btn-primary btn-lg" href="/lsapp/public/login" role="button">login</a> <a class="btn btn-success btn-lg" href="register" role="button">Register</a></p>
     </div>
 @endsection 
